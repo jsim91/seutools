8e4089e72a4b36229a03d1a02149d1e4d7a429c0
jsim91 <60735978+jsim91@users.noreply.github.com>
2024-10-01 13:19:49 -0500

    Update mast_src.R

--- inst/source_scripts/mast_src.R
+++ inst/source_scripts/mast_src.R
@@ -10,7 +10,7 @@ rem_MASTDETest <- function(
 ) {
   # https://github.com/satijalab/seurat/issues/3712#issuecomment-1379578940
   # Check for MAST
-  if (!PackageCheck('MAST', error = FALSE)) {
+  if (!rlang::check_installed('MAST', error = FALSE)) {
     stop("Please install MAST - learn more at https://github.com/RGLab/MAST")
   }
   group.info <- data.frame(row.names = c(cells.1, cells.2))
